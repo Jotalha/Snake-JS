4e66b4de30edd8d897d0b7b7ea9eadc1c62f043e
Jotalha <79644453+Jotalha@users.noreply.github.com>
2021-03-17 20:27:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -46,10 +46,10 @@ function update(event) {
 
 function iniciarJogo() {
 
-    if (snake[0].x > 14 * box && direction == "right") snake[0].x = 0;
-    if (snake[0].x < 1 && direction == "left") snake[0].x = 16 * box;
-    if (snake[0].y > 14 * box && direction == "down") snake[0].y = 0;
-    if (snake[0].y < 1 && direction == "up") snake[0].y = 16 * box;
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
     for (i = 1; i < snake.length; i++) {
         if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
@@ -86,4 +86,4 @@ function iniciarJogo() {
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 120);
+let jogo = setInterval(iniciarJogo, 100);
